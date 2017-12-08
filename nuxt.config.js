2de45647a8ef9f05491e0cf4200f6589067a7afc
wittayasutt <wittayasutt@gmail.com>
2017-12-08 10:48:42 +0700

    set env

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,58 @@
 module.exports = {
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: '8bitstick',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'play everything with 8bitstick' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
-  ** Customize the progress bar color
-  */
-  loading: { color: '#3B8070' },
-  /*
-  ** Build configuration
-  */
-  build: {
-    /*
-    ** Run ESLint on save
-    */
-    extend (config, ctx) {
-      if (ctx.dev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
-    }
-  }
+	head: {
+		title: '8bitstick',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{
+				hid: 'description',
+				name: 'description',
+				content: 'play everything with 8bitstick'
+			}
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+	},
+	css: ['bulma', '@/assets/scss/main.scss'],
+	loading: { color: '#E60012' },
+	build: {
+		postcss: {
+			plugins: {
+				'postcss-custom-properties': false
+			}
+		},
+		extend(config, ctx) {
+			if (ctx.dev && ctx.isClient) {
+				config.module.rules.push({
+					enforce: 'pre',
+					test: /\.(js|vue)$/,
+					// loader: 'eslint-loader',
+					exclude: /(node_modules)/
+				})
+			}
+
+			const vueLoader = config.module.rules.find(
+				({ loader }) => loader === 'vue-loader'
+			)
+			const { options: { loaders } } = vueLoader || { options: {} }
+			if (loaders) {
+				for (const loader of Object.values(loaders)) {
+					changeLoaderOptions(Array.isArray(loader) ? loader : [loader])
+				}
+			}
+			config.module.rules.forEach(rule => changeLoaderOptions(rule.use))
+		}
+	}
+}
+
+function changeLoaderOptions(loaders) {
+	if (loaders) {
+		for (const loader of loaders) {
+			if (loader.loader === 'sass-loader') {
+				Object.assign(loader.options, {
+					includePaths: ['./assets/scss'],
+					data: '@import "_variables.scss";'
+				})
+			}
+		}
+	}
 }
